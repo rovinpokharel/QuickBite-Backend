e78da0d7d1aee70ab9054582058a8615ae5697fe
Rovin <rovinpokharel123@gmail.com>
2025-03-16 23:45:55 +0545

    Added middleware

--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -1,9 +1,10 @@
 import express from "express";
 // import { createCurrentUser } from "../controllers/MyUserController";
 import MyUserController from "../controllers/MyUserController";
+import { jwtCheck } from "../middleware/auth";
 
 const router = express.Router();
 
-router.post("/", MyUserController.createCurrentUser);
+router.post("/", jwtCheck, MyUserController.createCurrentUser);
 
 export default router;
