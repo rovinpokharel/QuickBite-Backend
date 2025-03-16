f480756e78943a25ad1b36ac02dd9e7df3061ee9
Rovin <rovinpokharel123@gmail.com>
2025-03-16 22:07:11 +0545

    Create user API and added promise in MyUserController

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import express, {Request, Response} from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Connected to database!"));
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/my/user", myUserRoute);
 
 app.get("/test", async(req: Request, res: Response) => {
     res.json({message: "Hello!"});
