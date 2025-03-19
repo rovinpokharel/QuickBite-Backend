2db653aa9aaf3f685f29c250ec8ae3dcba23fb19
Rovin <rovinpokharel123@gmail.com>
2025-03-20 00:03:39 +0545

    Pre-populate user profile form

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -1,6 +1,22 @@
 import { Request, Response } from "express";
 import User from "../models/user";
 
+const getCurrentUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const currentUser = await User.findOne({ _id: req.userId });
+    if (!currentUser) {
+      // return res.status(404).json({ message: "User not found" });
+      res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(currentUser);
+  } catch (error) {
+    console.log(error);
+    // return res.status(500).json({ message: "Something went wrong" });
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
 const createCurrentUser = async (req: Request, res: Response): Promise<void> => {
     try {
       const { auth0Id } = req.body;
@@ -49,6 +65,7 @@ const createCurrentUser = async (req: Request, res: Response): Promise<void> => 
   };
 
   export default {
+    getCurrentUser,
     createCurrentUser,
     updateCurrentUser,
   };
